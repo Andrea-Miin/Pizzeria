fea4d5b7d47daf358d1272e4e86374ee5baa248c
saekkimiin <saekkimiin@gmail.com>
2018-11-07 10:19:37 +0100

    Pizza

---
+++ Pizzeria/Domain/Pizza.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain
+{
+    public class Pizza : EntityBase
+    {
+        [Required]
+        public string Name { get; set; }
+        public virtual ICollection<Ingredient> Id_Ingredient { get; set; }
+        public virtual ICollection<Comment> Id_Comment { get; set; }
+        [Required]
+        public byte[] Photo { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+    }
+}
