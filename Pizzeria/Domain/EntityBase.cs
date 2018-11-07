43f83f6dd6ea3d9a99eb2056ce7ed81ae578c257
PGarciaPhi <pgarcia.phi@gmail.com>
2018-11-07 12:54:22 +0100

    Creamos entidad base.

---
+++ Pizzeria/Domain/EntityBase.cs
@@ -0,0 +1,22 @@
+﻿namespace Domain
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class EntityBase : DbContext
+    {
+        public Guid Id { get; set; }
+
+        public EntityBase()
+            : base("name=EntityBase")
+        {
+        }
+
+        // Boolean. Manual validation data annotation.
+        public void IsValid() { }
+
+        // Errors dictionary. Crear nuevo error (throw).
+        public void Error() { }
+    }
+}
