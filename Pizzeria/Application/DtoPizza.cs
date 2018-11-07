28cc5e80efe3b6a5fe6d5c623b228709692378a5
saekkimiin <saekkimiin@gmail.com>
2018-11-07 10:21:51 +0100

    DtoPizza

--- Pizzeria/Application/DtoPizza.cs
+++ Pizzeria/Application/DtoPizza.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Domain;
 
 namespace Application
 {
-    public class DtoPizza
+    public class DtoPizza : Pizza
     {
+        public DtoPizza(Pizza pizza)
+        {
+            this.Name = pizza.Name;
+            this.Id_Ingredient = pizza.Id_Ingredient;
+            this.Id_Comment = pizza.Id_Comment;
+            this.Photo = pizza.Photo;
+            // Sustituir "5" por ConfigurationManager.AppSettings["Profit"]
+            this.Price = pizza.Id_Ingredient.Sum(c => c.Price) + decimal.Parse("5"); ;
+        }
     }
 }
