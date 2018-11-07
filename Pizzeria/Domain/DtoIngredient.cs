603d6b9d5417e41de708c11383c7133d11b7fefa
JoseYepes <Yepes_jose@hotmail.com>
2018-11-07 14:08:47 +0100

    Ingredientes, DtoIngredient y IContextPizzaShop

---
+++ Pizzeria/Domain/DtoIngredient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class DtoIngredient
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public Pizza Pizza { get; set; }
+        
+    }
+
+    
+}
