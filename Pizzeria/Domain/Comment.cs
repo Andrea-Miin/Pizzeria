7a1907b7487c4c2233644c593f7ca242a8e80165
Pablo García <pgarcia.phi@gmail.com>
2018-11-08 20:23:00 +0100

    Actualizar clases

--- Pizzeria/Domain/Comment.cs
+++ Pizzeria/Domain/Comment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,17 @@ namespace Domain
 {
     class Comment : EntityBase
     {
+        [Required]
         public string Text { get; set; }
-        public byte Punctuacion { get; set; }
+        [Required]
+        public int Score { get; set; }
+        [Required]
         public DateTime Date { get; set; }
+        [Required]
+        public string User { get; set; }
+
+        [Required]
         public int Id_Pizza { get; set; }
+        public Pizza Pizza { get; set; }
     }
 }
--- Pizzeria/Infrastructure/ContextPizzaShop.cs
+++ Pizzeria/Infrastructure/ContextPizzaShop.cs
@@ -4,11 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
+using Domain;
 
 namespace Infrastructure
 {
-    class ContextPizzaShop : DbContext, IContextPizzaShop
+    public class ContextPizzaShop : DbContext, IContextPizzaShop
     {
-        
+        public IDbSet<Pizza> Pizzas { get; set; }
+        public IDbSet<Ingredient> Ingredients { get; set; }
+        public IDbSet<Comment> Comments { get; set; }
+
+        public ContextPizzaShop()
+            : base("name=ContextPizzaShop")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pizza>()
+                        .HasMany<Ingredient>(x => x.Collection_Ingredient)
+                        .WithMany(y => y.Collection_Pizzas)
+                        .Map(xy =>
+                        {
+                            xy.MapLeftKey("Pizza_Id");
+                            xy.MapRightKey("Ingredient_Id");
+                            xy.ToTable("PizzaIngredients");
+                        });
+
+            modelBuilder.Entity<Pizza>()
+                        .HasMany<Comment>(x => x.Collection_Comment)
+                        .WithRequired(y => y.Pizza)
+                        .HasForeignKey<int>(y => y.Id_Pizza);
+        }
+
+        int IUnitOfWork.SaveChanges()
+        {
+            return this.SaveChanges();
+        }
     }
 }
