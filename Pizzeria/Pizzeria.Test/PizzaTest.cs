e3a23c8f36528053ec52ebecb4ad0cbf1b120e78
saekkimiin <saekkimiin@gmail.com>
2018-11-09 00:43:22 +0100

    PizzaTest

---
+++ Pizzeria/Pizzeria.Test/PizzaTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pizzeria.Test
+{
+    [TestClass]
+    public class PizzaTest
+    {
+        [TestMethod]
+        public void Price_NegativeResult()
+        {
+            var pizza = new Pizza();
+            var price = pizza.Price();
+
+            Assert.IsTrue(price < 0);
+        }
+        [TestMethod]
+        public void CreatePizza_NullValues(DtoPizza dtoPizza)
+        {
+            var pizza = Pizza.CreatePizza(dtoPizza);
+
+            Assert.IsNull(pizza.Name);
+            Assert.IsNull(pizza.Photo);
+            Assert.IsNull(pizza.Id_Ingredient);
+        }
+    }
+}
