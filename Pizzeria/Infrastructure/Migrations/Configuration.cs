f4b2b8d6c2c12d97b6aa323aa3856115496f13f4
saekkimiin <saekkimiin@gmail.com>
2018-11-09 01:00:56 +0100

    Editado Seed

--- Pizzeria/Infrastructure/Migrations/Configuration.cs
+++ Pizzeria/Infrastructure/Migrations/Configuration.cs
@@ -14,7 +14,16 @@ namespace Infrastructure.Migrations
 
         protected override void Seed(Infrastructure.ContextPizzaShop context)
         {
-
+            if (!context.Ingredients.Any())
+            {
+                context.Ingredients.AddOrUpdate
+                (
+                    new Domain.Ingredient { Name = "Masa", Price = 0.5M },
+                    new Domain.Ingredient { Name = "Queso", Price = 0.4M },
+                    new Domain.Ingredient { Name = "Tomate", Price = 0.2M },
+                    new Domain.Ingredient { Name = "Piña", Price = 0.3M }
+                );
+            }
         }
     }
 }
